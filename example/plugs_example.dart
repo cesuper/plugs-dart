badb5bd3d5077a52dfa8a7b472279e3fa081591c
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-08-06 09:08:29 +0200

    initial commit added

---
+++ example/plugs_example.dart
@@ -0,0 +1,6 @@
+import 'package:plugs/plugs.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/plugs.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library plugs;
+
+export 'src/plugs_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
