a4cab368d40b25aa15f9fe786526bfedda9e3d3e
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-08-07 17:10:38 +0200

    comments added

--- example/plugs_example.dart
+++ example/plugs_example.dart
@@ -5,6 +5,7 @@ void main() async {
   var plug = Scp412('192.168.100.111:8080');
   var group = await plug.readGroup();
   var socket = plug.socket;
+
   print(await socket.addresses());
   print(await socket.h28());
   print(await socket.h43());
--- lib/socket/Socket.dart
+++ lib/socket/Socket.dart
@@ -8,24 +8,27 @@ const CONST_SOCKET_API_43 = '/api/socket/43.cgi';
 
 class Socket {
   // network address
-  final String address;
+  final String _address;
 
-  Socket(this.address);
+  Socket(String address) : _address = address;
 
+  /// Returns all the 1Wire device addresses connected
   Future<List<String>> addresses() async {
-    var uri = Uri.http('$address', CONST_SOCKET_API);
+    var uri = Uri.http('$_address', CONST_SOCKET_API);
     var r = await http.get(uri);
     return List<String>.from(jsonDecode(r.body));
   }
 
+  /// Returns all the h28 devices connected
   Future<List<String>> h28() async {
-    var uri = Uri.http('$address', CONST_SOCKET_API_28);
+    var uri = Uri.http('$_address', CONST_SOCKET_API_28);
     var r = await http.get(uri);
     return List<String>.from(jsonDecode(r.body));
   }
 
+  /// Returns all the h43 devices connected
   Future<List<String>> h43() async {
-    var uri = Uri.http('$address', CONST_SOCKET_API_43);
+    var uri = Uri.http('$_address', CONST_SOCKET_API_43);
     var r = await http.get(uri);
     return List<String>.from(jsonDecode(r.body));
   }
