e38eafcc858e122de04a02a267b4cff26c99fa49
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-09-01 15:44:35 +0200

    .

---
+++ test/smp_sampling_test.dart
@@ -0,0 +1,40 @@
+import 'package:plugs/smp/Smp.dart';
+import 'package:plugs/smp/SmpTrigger.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  final tSampling = 5000;
+
+  var plugs = <Smp>[
+    Smp('192.168.100.111:8081'),
+    Smp('192.168.100.111:8082'),
+  ];
+
+  group('Multiple-Sampling', () {
+    test('Trigger', () async {
+      // create common trigger
+      var trigger = SmpTrigger(DateTime.now().microsecond, 100, tSampling);
+
+      try {
+        // send request and wait for response
+        var results = await getResults(plugs, trigger);
+        print(results);
+      } catch (e) {
+        print('plug error');
+      }
+    });
+  });
+}
+
+Future<List<int>> getResults(List<Smp> plugs, SmpTrigger trigger) async {
+  // trigger every plug
+  await Future.wait(plugs.map((e) => e.setTrigger(trigger)), eagerError: true);
+
+  // wait a bit
+  await Future.delayed(Duration(milliseconds: trigger.tSampling + 100));
+
+  // get results
+  var results = await Future.wait(plugs.map((e) => e.data()), eagerError: true);
+
+  return results.map((e) => e.ts).toList();
+}
