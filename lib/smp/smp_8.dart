679564dd3c658d332afbdf8d043469115d6a31c0
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-09-29 14:06:38 +0200

    Update smp_8.dart

--- lib/smp/smp_8.dart
+++ lib/smp/smp_8.dart
@@ -2,7 +2,6 @@ import 'package:plugs/smp/smp.dart';
 
 class Smp8 extends Smp {
   // max sensors
-  static final int SMP8_MAX_SENSORS = 8;
 
-  Smp8(String address) : super(address, SMP8_MAX_SENSORS);
+  Smp8(String address) : super(address, 8);
 }
