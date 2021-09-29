749aa5e624fe8c5c251bd7913ff20b928143f699
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-09-29 15:17:16 +0200

    .

--- lib/cp/cp_plug.dart
+++ lib/cp/cp_plug.dart
@@ -12,6 +12,17 @@ class CpPlug extends Smp {
   //
   static const String model = 'CP';
 
+  // List of dummy channels
+  static final channels = <CpChannel>[
+    CpChannel('5OCUGBGR', 10.0, name: 'Ch 1', index: 0, cavity: 1, position: 1),
+    CpChannel('5OETIN28', 10.0, name: 'Ch 2', index: 1, cavity: 1, position: 2),
+    CpChannel('50OAGBHN', 10.0, name: 'Ch 3', index: 2, cavity: 1, position: 3),
+    CpChannel('577KBN88', 10.0, name: 'Ch 4', index: 3, cavity: 2, position: 1),
+    CpChannel('6M2OGBHP', 10.0, name: 'Ch 5', index: 4, cavity: 2, position: 2),
+    CpChannel('64UF4NBU', 10.0, name: 'Ch 6', index: 5, cavity: 2, position: 3),
+    CpChannel('5CWKBN8H', 10.0, name: 'Ch 7', index: 6, cavity: 3, position: 1),
+  ];
+
   //
   CpPlug(String address, int maxSensors) : super(address, maxSensors);
 
