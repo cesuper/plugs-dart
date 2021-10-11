db2b73d1bb52cc16af39c92c8a288ec1a543fe34
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-10-11 17:31:03 +0200

    .

--- lib/flw/flw_snapshot.dart
+++ lib/flw/flw_snapshot.dart
@@ -4,18 +4,23 @@ import 'package:plugs/flw/flw_sensor_data.dart';
 
 class FlwSnapshot {
   //
+  final int ts;
+
+  //
   final List<FlwSensorData> sensors;
 
-  FlwSnapshot(this.sensors);
+  FlwSnapshot(this.ts, this.sensors);
 
   Map<String, dynamic> toMap() {
     return {
+      'ts': ts,
       'sensors': sensors.map((x) => x.toMap()).toList(),
     };
   }
 
   factory FlwSnapshot.fromMap(Map<String, dynamic> map) {
     return FlwSnapshot(
+      map['ts'],
       List<FlwSensorData>.from(
           map['sensors']?.map((x) => FlwSensorData.fromMap(x))),
     );
@@ -27,5 +32,5 @@ class FlwSnapshot {
       FlwSnapshot.fromMap(json.decode(source));
 
   @override
-  String toString() => 'FlwSnapshot(sensors: $sensors)';
+  String toString() => 'FlwSnapshot(ts: $ts, sensors: $sensors)';
 }
