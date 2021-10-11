e826298b4a7edc8dee177e6ce25a014ee5888230
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-10-11 16:26:46 +0200

    .

--- lib/flw/flw_const.dart
+++ lib/flw/flw_const.dart
@@ -1,1 +1,6 @@
+import 'package:plugs/flw/flw_channel.dart';
 
+final flwChannels = <FlwChannel>[
+  FlwChannel('994559880192100081', 'Channel 1'),
+  FlwChannel('994559880192100105', 'Channel 2'),
+];
