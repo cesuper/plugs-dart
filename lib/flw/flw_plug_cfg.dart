12265cd3a91667a129feaa06da0fbde9ad48ff22
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-10-12 09:21:34 +0200

    .

--- lib/flw/flw_plug_cfg.dart
+++ lib/flw/flw_plug_cfg.dart
@@ -2,11 +2,11 @@ import 'dart:convert';
 
 import 'flw_sensor.dart';
 
-class FlwCfg {
+class FlwPlugCfg {
   // list of sensors
   final List<FlwSensor> sensors;
 
-  FlwCfg(this.sensors);
+  FlwPlugCfg(this.sensors);
 
   Map<String, dynamic> toMap() {
     return {
@@ -14,16 +14,17 @@ class FlwCfg {
     };
   }
 
-  factory FlwCfg.fromMap(Map<String, dynamic> map) {
-    return FlwCfg(
+  factory FlwPlugCfg.fromMap(Map<String, dynamic> map) {
+    return FlwPlugCfg(
       List<FlwSensor>.from(map['sensors']?.map((x) => FlwSensor.fromMap(x))),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory FlwCfg.fromJson(String source) => FlwCfg.fromMap(json.decode(source));
+  factory FlwPlugCfg.fromJson(String source) =>
+      FlwPlugCfg.fromMap(json.decode(source));
 
   @override
-  String toString() => 'FlwCfg(sensors: $sensors)';
+  String toString() => 'FlwPlugCfg(sensors: $sensors)';
 }
