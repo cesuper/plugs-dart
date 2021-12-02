ffb3758f0ad70269703e6e4b7055ee3a843dc701
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-12-02 12:40:25 +0100

    .

--- lib/discovery.dart
+++ lib/discovery.dart
@@ -1,19 +1,65 @@
-// import 'dart:io';
-// import 'dart:convert';
+import 'dart:convert';
+import 'dart:io';
 
-// main() {
-//   var DESTINATION_ADDRESS = InternetAddress("x.y.z.255");
+import 'package:plugs/plug/info.dart';
 
-//   RawDatagramSocket.bind(InternetAddress.anyIPv4, 8889)
-//       .then((RawDatagramSocket udpSocket) {
-//     udpSocket.broadcastEnabled = true;
-//     udpSocket.listen((e) {
-//       Datagram dg = udpSocket.receive();
-//       if (dg != null) {
-//         print("received ${dg.data}");
-//       }
-//     });
-//     List<int> data = utf8.encode('TEST');
-//     udpSocket.send(data, DESTINATION_ADDRESS, 8889);
-//   });
-// }
+/// Utility class for discovering plugs in the subnet
+/// same as [localAddress]. A directed broadcast udp
+/// frame is sent and waits for reply until [timeout]
+/// elapsed.
+class Discovery {
+  ///
+  static Future<List<Info>> discover(
+    InternetAddress localAddress,
+    Duration timeout,
+  ) async {
+    //
+    var infos = <Info>[];
+
+    // todo handle error
+    // bind to any port
+    var socket = await RawDatagramSocket.bind(localAddress, 0);
+
+    // enable broadcast
+    socket.broadcastEnabled = true;
+
+    //
+    socket.listen((e) {
+      if (e == RawSocketEvent.read) {
+        // read a chunk
+        Datagram? dg = socket.receive();
+
+        // check if dg available
+        if (dg != null) {
+          // get the string content from the datagram
+          var sub = dg.data.takeWhile((value) => value != 0).toList();
+
+          //
+          var str = utf8.decode(sub, allowMalformed: true);
+
+          //
+          infos.add(Info.fromJson(str));
+        }
+      }
+    });
+    //
+    var targetRawAddress = localAddress.rawAddress..[3] = 0xFF;
+
+    var destinationAddress = InternetAddress.fromRawAddress(
+      targetRawAddress,
+      type: InternetAddressType.IPv4,
+    );
+
+    // send request
+    List<int> data = [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+    socket.send(data, destinationAddress, 6060);
+
+    // wait
+    await Future.delayed(timeout);
+
+    //
+    socket.close();
+
+    return infos;
+  }
+}
--- test/discovery_test.dart
+++ test/discovery_test.dart
@@ -1,48 +1,58 @@
 // ignore_for_file: avoid_print
 
-import 'dart:convert';
 import 'dart:io';
-import 'package:plugs/plug/info.dart';
+
+import 'package:plugs/discovery.dart';
 
 void main() async {
+  //
   var destinationAddress = InternetAddress("192.168.100.255");
 
-  //
-  var socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+  var devices = await Discovery.discover(
+    InternetAddress('192.168.100.118', type: InternetAddressType.IPv4),
+    const Duration(seconds: 2),
+  );
 
-  //
-  socket.broadcastEnabled = true;
+  for (var device in devices) {
+    print(device);
+  }
 
-  //
-  socket.listen((event) {});
+  // //
+  // var socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
 
-  RawDatagramSocket.bind(InternetAddress.anyIPv4, 0)
-      .then((RawDatagramSocket udpSocket) {
-    udpSocket.broadcastEnabled = true;
-    udpSocket.listen((e) {
-      if (e == RawSocketEvent.read) {
-        // read a chunk
-        Datagram? dg = udpSocket.receive();
+  // //
+  // socket.broadcastEnabled = true;
 
-        //
-        var data = dg!.data;
+  // //
+  // socket.listen((event) {});
 
-        //
-        var sub = data.takeWhile((value) => value != 0).toList();
+  // RawDatagramSocket.bind(InternetAddress.anyIPv4, 0)
+  //     .then((RawDatagramSocket udpSocket) {
+  //   udpSocket.broadcastEnabled = true;
+  //   udpSocket.listen((e) {
+  //     if (e == RawSocketEvent.read) {
+  //       // read a chunk
+  //       Datagram? dg = udpSocket.receive();
 
-        //
-        var str = utf8.decode(sub, allowMalformed: true);
+  //       //
+  //       var data = dg!.data;
 
-        //
-        var info = Info.fromJson(str);
+  //       //
+  //       var sub = data.takeWhile((value) => value != 0).toList();
 
-        //
-        print(info);
-      }
-    });
-    List<int> data = [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
-    udpSocket.send(data, destinationAddress, 6060);
-  });
+  //       //
+  //       var str = utf8.decode(sub, allowMalformed: true);
 
-  await Future.delayed(const Duration(seconds: 5));
+  //       //
+  //       var info = Info.fromJson(str);
+
+  //       //
+  //       print(info);
+  //     }
+  //   });
+  //   List<int> data = [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+  //   udpSocket.send(data, destinationAddress, 6060);
+  // });
+
+  // await Future.delayed(const Duration(seconds: 5));
 }
