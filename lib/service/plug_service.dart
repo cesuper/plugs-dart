439497150bb68f8509d52bc8f2e85f2bb9168fdf
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-12-18 14:59:37 +0100

    .

--- lib/service/plug_service.dart
+++ lib/service/plug_service.dart
@@ -6,10 +6,7 @@ import 'package:plugs/discovery.dart';
 import 'package:plugs/plugs/plug/info.dart';
 
 //
-typedef PlugConnectedCallback = void Function();
-
-//
-typedef PlugDisconnectedCallback = void Function();
+typedef NetworkStateChangedCallback = void Function(List<Info> plugs);
 
 /// TODO: provide detailed description about device service
 ///
@@ -30,10 +27,10 @@ class PlugService {
   final int port;
 
   // callback
-  final PlugConnectedCallback? onConnected;
+  final NetworkStateChangedCallback? onConnected;
 
   // callback
-  final PlugDisconnectedCallback? onDisconnected;
+  final NetworkStateChangedCallback? onDisconnected;
 
   // list of discovered devices
   List<Info> _devices = [];
@@ -73,11 +70,18 @@ class PlugService {
 
         try {
           // start discovery and wait for result
-          _devices = await Discovery.discover(
+          var recent = await Discovery.discover(
             localAddress,
             timeout: timeout,
             port: port,
           );
+          //
+          _searchForNew(_devices, recent);
+          //
+          _searchForDisconnected(_devices, recent);
+
+          // update devices
+          _devices = recent;
         } on SocketException catch (e, stackTrace) {
           // log socket exceptions
           logger?.e('Socket Exception, check port?', e, stackTrace);
@@ -89,5 +93,47 @@ class PlugService {
         _isDiscovering = false;
       }
     });
+  }
+
+  /// Function to filter new plugs.
+  /// Plugs are considered as new when not present in the previous
+  /// discovery result.
+  void _searchForNew(List<Info> previous, List<Info> recent) {
+    // plug is not new when the plug serial and ip address remains unchanged
+
+    // list of new plugs
+    var list = <Info>[];
+    for (var r in recent) {
+      // is r exists in the last dicovery?
+      var isExising = previous.any((e) =>
+          (e.network.ip == r.network.ip) &&
+          (e.hardware.serial == r.hardware.serial));
+      // if not, then add as new plug
+      if (isExising == false) {
+        list.add(r);
+      }
+    }
+    //
+    onConnected!(list);
+  }
+
+  /// Function to find disconnected plugs
+  /// Plugs are considered as disconnected when existed in the previous but
+  /// not in recent
+  void _searchForDisconnected(List<Info> previous, List<Info> recent) {
+    // list of new plugs
+    var list = <Info>[];
+    for (var p in previous) {
+      // is r exists in the last dicovery?
+      var isExising = recent.any((e) =>
+          (e.network.ip == p.network.ip) &&
+          (e.hardware.serial == p.hardware.serial));
+      // if not, then add as new plug
+      if (isExising == false) {
+        list.add(p);
+      }
+    }
+    //
+    onDisconnected!(list);
   }
 }
--- test/plug_service_test.dart
+++ test/plug_service_test.dart
@@ -16,8 +16,25 @@ final logger = Logger();
 void main() async {
   test('', () async {
     // create and start service
-    var service = PlugService(localAddress)..start();
+    var service = PlugService(
+      localAddress,
+      onConnected: (plugs) {
+        for (var element in plugs) {
+          // ignore: avoid_print
+          print(
+              'Connected: ${element.hardware.serial} : ${element.network.ip}');
+        }
+      },
+      onDisconnected: (plugs) {
+        for (var element in plugs) {
+          // ignore: avoid_print
+          print(
+              'Disconnected: ${element.hardware.serial} : ${element.network.ip}');
+        }
+      },
+    )..start();
 
     //
-  });
+    await Future.delayed(const Duration(seconds: 45));
+  }, timeout: const Timeout(Duration(seconds: 50)));
 }
