fed5b8df8cde526754a1246975db7b5f423861be
Daniel Vamos <daniel.vamos@cavityeye.com>
2021-12-20 15:49:41 +0100

    .

---
+++ lib/discovery.dart
@@ -0,0 +1,185 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:plugs/plugs/plug/info.dart';
+
+//
+typedef ConnectionStateChangedCallback = Function(Info info, bool isConnected);
+
+/// TODO: in service mode the timer should wait for period before start
+/// TODO: provide detailed description about device service
+///
+class Discovery {
+  /// Port used by plugs to recieve discovery request
+  static const int remotePort = 6060;
+
+  /// Discovery request
+  static final request = <int>[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+
+  // local address where plugs expected to be
+  final InternetAddress localAddress;
+
+  // discovery response timeout
+  final Duration timeout;
+
+  // result of the last discovery
+  List<Info> _devices = [];
+
+  // list of devices discovered
+  List<Info> get listeners => _devices;
+
+  // timer for periodically check device presence
+  Timer? _timer;
+
+  // flag indicating discovery is in progress
+  bool _isDiscovering = false;
+
+  //
+  final ConnectionStateChangedCallback? onStateChanged;
+
+  /// Returns a new instance of device service.
+  /// [localAddress] local interface address where the plugs being searched
+  /// [period] device scan period
+  /// [timeout] timeout for discovery response
+  /// [port] port to bind discovery socket, default is 0
+  Discovery(
+    this.localAddress, {
+    this.onStateChanged,
+    this.timeout = const Duration(seconds: 1),
+  });
+
+  ///
+  Future<List<Info>> discover() async {
+    // check if progress
+    if (!_isDiscovering) {
+      // set discovery flag
+      _isDiscovering = true;
+
+      // scan devices
+      var result = await _discover();
+
+      // handle new connections
+      _checkConnections(_devices, result).forEach((e) {
+        onStateChanged?.call(e, true);
+      });
+
+      // handle removals
+      _checkRemovals(_devices, result).forEach((e) {
+        onStateChanged?.call(e, false);
+      });
+
+      // update devices
+      _devices = result;
+
+      // reset flag
+      _isDiscovering = false;
+    }
+
+    // return devices discovered when service is running
+    return _devices;
+  }
+
+  /// Starts a periodic timer to check devices on the network.
+  /// [onStateChanged] callback is fired when new device discovered or lost
+  void start({Duration period = const Duration(seconds: 3)}) {
+    // setup timer for period calls
+    _timer = Timer.periodic(period, (timer) async => await discover());
+  }
+
+  /// Stops timer
+  void stop() => _timer?.cancel();
+
+  /// List device connections.
+  /// Connection is new, when an address presents in [discovered] but
+  /// not available in [recent]
+  List<Info> _checkConnections(List<Info> recent, List<Info> discovered) {
+    // list of new plugs
+    var found = <Info>[];
+    for (var device in discovered) {
+      // check if address is new since the last discovery
+      var exists = recent.any((e) => (e.network.ip == device.network.ip));
+      // if not, then add as new plug
+      if (exists == false) {
+        found.add(device);
+      }
+    }
+    return found;
+  }
+
+  /// List device removals
+  /// Connection is removed, when an address presents in [recent] but not
+  /// available in [discovered]
+  List<Info> _checkRemovals(List<Info> recent, List<Info> discovered) {
+    // list of lost
+    var lost = <Info>[];
+    for (var device in recent) {
+      // check if address is lost since the last dicovery
+      var exists = discovered.any((e) => (e.network.ip == device.network.ip));
+      // if not, then add as new plug
+      if (exists == false) {
+        lost.add(device);
+      }
+    }
+    return lost;
+  }
+
+  /// Starts the discovery process
+  /// [localAddress] local interface address where the plugs being searched
+  /// [timeout] time to wait for discovery response
+  /// [port] port number to bind, 0 = random port
+  ///
+  Future<List<Info>> _discover() async {
+    //
+    var result = <Info>[];
+
+    // todo handle error
+    // bind to any port
+    var socket = await RawDatagramSocket.bind(localAddress, 0);
+
+    // enable broadcast
+    socket.broadcastEnabled = true;
+
+    //
+    socket.listen(
+      (e) {
+        if (e == RawSocketEvent.read) {
+          // read a chunk
+          Datagram? dg = socket.receive();
+
+          // check if dg available
+          if (dg != null) {
+            // get the string content from the datagram
+            var sub = dg.data.takeWhile((value) => value != 0).toList();
+
+            //
+            var str = utf8.decode(sub, allowMalformed: true);
+
+            //
+            result.add(Info.fromJson(str));
+          }
+        }
+      },
+    );
+    // construct direct broadcast address from local address
+    var targetRawAddress = localAddress.rawAddress..[3] = 0xFF;
+
+    // create destination address from direct broadcast address
+    var destinationAddress = InternetAddress.fromRawAddress(
+      targetRawAddress,
+      type: InternetAddressType.IPv4,
+    );
+
+    // send discovery request
+    socket.send(request, destinationAddress, remotePort);
+
+    // wait for reply
+    await Future.delayed(timeout);
+
+    //
+    socket.close();
+
+    // return the discovery result
+    return result;
+  }
+}
--- test/discovery_test.dart
+++ test/discovery_test.dart
@@ -1,35 +1,37 @@
 // ignore_for_file: avoid_print
 
 import 'dart:io';
-import 'package:plugs/discovery/discovery.dart';
-import 'package:plugs/discovery/discovery_service.dart';
+import 'package:plugs/discovery.dart';
 import 'package:test/scaffolding.dart';
 
-final localAddress =
-    InternetAddress('192.168.100.118', type: InternetAddressType.IPv4);
+final localAddress = InternetAddress(
+  '192.168.100.118',
+  type: InternetAddressType.IPv4,
+);
 
 void main() async {
-  test('Function', () async {
+  test('Simple', () async {
     //
-    var devices = await Discovery.discover(localAddress);
+    final service = Discovery(localAddress);
+    final result = await service.discover();
 
     // print found
-    for (var device in devices) {
+    for (var device in result) {
       print(device.network.ip);
     }
   });
 
   test('Service', () async {
     //
-    var service = DiscoveryService(
+    final service = Discovery(
       localAddress,
       onStateChanged: (info, isConnected) =>
-          print('${info.network.ip} $isConnected'),
+          print(info.network.ip + (isConnected ? ' Connected' : ' Removed')),
     );
-
-    // start service
+    print('Service Started');
     service.start();
-
-    await Future.delayed(const Duration(seconds: 25));
-  });
+    await Future.delayed(const Duration(seconds: 20));
+    print('Service Stopped');
+    service.stop();
+  }, timeout: const Timeout(Duration(seconds: 25)));
 }
