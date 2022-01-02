6170c21ff973502e24d40257d5cdc29aa43ad81e
Daniel Vamos <daniel.vamos@cavityeye.com>
2022-01-02 14:38:27 +0100

    Update ceflash_test.dart

--- test/ceflash_test.dart
+++ test/ceflash_test.dart
@@ -10,20 +10,11 @@ import 'package:plugs/discovery.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
-// local address
+// system local address
 final localAddress = InternetAddress('192.168.100.118');
 
-// target ip address
-final remoteAddress = InternetAddress('192.168.100.102');
-
-// 94-FB-A7-51-00-3B
-const remoteMac = '94-fb-a7-51-00-3b';
-
-// 94-FB-A7-51-00-8C
-//const remoteMac = '94-fb-a7-51-00-8c';
-
 // firmware path
-const path = 'assets/sfp9-r2-1.5.1.bin';
+const path = 'assets/sfp9-r2-1.7.1.bin';
 
 const bootpRequestTimeout = Duration(seconds: 25);
 const tftpRequestTimmeout = Duration(seconds: 10);
@@ -80,13 +71,19 @@ void main() async {
   });
 
   test('Bootloader mode', () async {
+    // target mac address to be flashed
+    const targetMac = '94-fb-a7-51-00-8c';
+
+    // ip address to assign to the target for the duration of flashing
+    const targetAddress = '192.168.100.109';
+
     // Target is expected to be in Bootloader mode and periodically
     // sendins BOOTP requests indicating the device is ready to update itself
     // with the new firmware
     var result = await CeFlash.update(
       localAddress,
-      remoteAddress,
-      remoteMac,
+      InternetAddress(targetAddress),
+      targetMac,
       firmware,
       magicPacket: false,
       timeout: const Duration(seconds: 5),
@@ -100,10 +97,24 @@ void main() async {
   test('App mode', () async {
     // Target is expected to be in App mode, waiting for magic packet to
     // boot in bootloader mode to accept the firmware.
+
+    // target mac address to run the test on
+    const targetMac = '08-00-28-5a-8f-a1';
+
+    // get available devices
+    final devices = await Discovery.discover(localAddress);
+
+    // find device
+    final device =
+        devices.entries.firstWhere((element) => element.value.mac == targetMac);
+
+    // target address
+    final targetAddress = InternetAddress(device.key);
+
     var result = await CeFlash.update(
       localAddress,
-      remoteAddress,
-      remoteMac,
+      targetAddress,
+      targetMac,
       firmware,
       magicPacket: true,
       timeout: const Duration(seconds: 5),
