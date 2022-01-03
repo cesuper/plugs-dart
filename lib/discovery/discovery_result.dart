521176e199890aaa32ff8989e7d1d139074bfd3c
Daniel Vamos <daniel.vamos@cavityeye.com>
2022-01-03 17:48:03 +0100

    Update discovery_result.dart

--- lib/discovery/discovery_result.dart
+++ lib/discovery/discovery_result.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:plugs/plugs/plug/info.dart';
 
 class DiscoveryResult {
@@ -8,4 +10,23 @@ class DiscoveryResult {
   final Info info;
 
   DiscoveryResult(this.address, this.info);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'address': address,
+      'info': info.toMap(),
+    };
+  }
+
+  factory DiscoveryResult.fromMap(Map<String, dynamic> map) {
+    return DiscoveryResult(
+      map['address'] ?? '',
+      Info.fromMap(map['info']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory DiscoveryResult.fromJson(String source) =>
+      DiscoveryResult.fromMap(json.decode(source));
 }
