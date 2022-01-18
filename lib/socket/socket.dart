7bcd5014a12a62e029ddc3852cf4e1a75361c063
Daniel Vamos <daniel.vamos@cavityeye.com>
2022-01-18 20:45:40 +0100

    .

--- lib/socket/socket.dart
+++ lib/socket/socket.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: file_names
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:plugs/api_exception.dart';
 import 'package:plugs/socket/memory.dart';
-import 'h43.dart';
+import 'package:universal_io/io.dart';
 
 class Socket {
   // network address
@@ -21,28 +22,41 @@ class Socket {
   /// Return socket memory state
   Future<Memory> getMemory() async {
     final uri = Uri.http(_address, '/api/socket/memory/info.cgi');
-    final r = await http.get(uri);
-    return Memory.fromJson(r.body);
+    final response = await http.get(uri);
+
+    //
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, response.body);
+    }
+
+    return Memory.fromJson(response.body);
   }
 
   ///
-
   Future<String> readMemory() async {
     final uri = Uri.http(_address, '/api/socket/memory.cgi');
-    final r = await http.get(uri);
-    return r.body;
+    final response = await http.get(uri);
+
+    //
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, response.body);
+    }
+
+    return response.body;
   }
 
   ///
-  ///
-  ///
-  Future<int> writeMemory(String content, String address) async {
+  Future<void> writeMemory(Object content) async {
     var uri = Uri.http(_address, '/api/socket/memory.cgi');
-    var r = await http.post(
+    var response = await http.post(
       uri,
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'address': address, 'content': content}),
+      body: jsonEncode(content),
     );
-    return r.statusCode;
+
+    //
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, response.body);
+    }
   }
 }
--- test/socket_test.dart
+++ test/socket_test.dart
@@ -22,4 +22,37 @@ void main() async {
     final content = await socket.readMemory();
     print(content);
   });
+
+  test('Write Socket Memory', () async {
+    final map = <String, dynamic>{
+      "sensors": [
+        {
+          "serial": "994559880194600012",
+          "name": "Channel 1",
+          "group": "",
+          "dir": ""
+        },
+        {
+          "serial": "994559880192100003",
+          "name": "Channel 2",
+          "group": "",
+          "dir": ""
+        },
+        {
+          "serial": "994559880185000035",
+          "name": "Channel 3",
+          "group": "",
+          "dir": ""
+        },
+        {
+          "serial": "994559880192100021",
+          "name": "Channel 4",
+          "group": "",
+          "dir": ""
+        }
+      ]
+    };
+
+    final content = await socket.writeMemory(map);
+  });
 }
